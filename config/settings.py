e077d811d97e225d387893f643dd7381f45119c2
Hanieh <ahmadihanieh02@gmail.com>
2026-07-11 18:13:48 -0500

    Add accounts app and update settings

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     #local
     'post',
+    'accounts',
+
     
 
 ]
@@ -126,3 +128,6 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
