1783f75cac22b60341899639b8ac153d8d80ffd4
Hanieh <ahmadihanieh02@gmail.com>
2026-07-24 20:46:10 -0500

    crispy forms are added

--- config/settings.py
+++ config/settings.py
@@ -38,13 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party 
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     #local
     'post',
-    'accounts',
-
-    
+    'accounts',   
 
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
